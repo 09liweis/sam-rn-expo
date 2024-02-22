2e1cf552f0abc85b16a67fe13bbac592658ee77f
Sam Li <weisen.li@hotmail.com>
2024-02-22 21:40:34 +0000

    add fetch movie method

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
 export default function App() {
+  const getMoviesFromApiAsync = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://samliweisen.onrender.com/api/movies?limit=100",
+      );
+      const json = await response.json();
+      setMovies(json.movies);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMoviesFromApiAsync();
+  }, []);
   return (
     <View style={styles.container}>
-      <ActivityIndicator animating={true} size={"large"} />
-      <Text style={styles.text}>Thats cool</Text>
-      <Text>New Text</Text>
+      <Text style={styles.text}>Movies</Text>
+      {loading ? (
+        <ActivityIndicator animating={true} size={"medium"} />
+      ) : (
+        <Text>{movies.length}</Text>
+      )}
       <Image
         source={{
           uri: "https://m.media-amazon.com/images/M/MV5BMTY3Nzg2NjA1OF5BMl5BanBnXkFtZTgwMjY5NTU1MzI@._V1_FMjpg_UX309_.jpg",
