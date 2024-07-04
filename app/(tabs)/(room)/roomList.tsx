39cb20e9c301a3927cca10f8c3a3a6f41740c06a
Sam Li <weisen.li@hotmail.com>
2024-07-04 19:05:41 +0000

    style add room button

--- app/(tabs)/(room)/roomList.tsx
+++ app/(tabs)/(room)/roomList.tsx
@@ -35,7 +35,9 @@ const RoomList = () => {
         keyExtractor={(item: Room) => item._id}
         contentContainerStyle={styles.list}
       />
-      <Button onPress={() => router.push("(room)/roomForm")} title="Add Room" />
+      <Link style={styles.addButton} href={`(room)/roomForm`}>
+        Add
+      </Link>
     </>
   );
 };
@@ -59,6 +61,20 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 8,
+  },
+  addButton: {
+    backgroundColor: "red",
+    color: "#fff",
+    borderRadius: 8,
+    padding: 16,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+    position: "absolute",
+    bottom: 20,
+    right: 20,
   },
 });
 
