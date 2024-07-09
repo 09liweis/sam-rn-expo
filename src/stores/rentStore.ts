152ede98e0a122b46ccb16929830b81f0a66e25a
Sam Li <weisen.li@hotmail.com>
2024-07-09 18:34:49 +0000

    move roomStore type to roomType

--- src/stores/rentStore.ts
+++ src/stores/rentStore.ts
@@ -1,14 +1,9 @@
 import { ROOM_API } from "src/constant/api";
-import { Room } from "src/types/roomType";
+import { RoomStore } from "src/types/roomType";
 import { fetchData } from "utils";
 import { create } from "zustand";
 
-type Store = {
-  rentRoomList: Array<Room>;
-  fetchRoomList: () => void;
-};
-
-const useRentStore = create<Store>()((set) => ({
+const useRentStore = create<RoomStore>()((set) => ({
   rentRoomList: [],
   fetchRoomList: async () => {
     const response = await fetchData({ url: ROOM_API });
--- src/types/roomType.ts
+++ src/types/roomType.ts
@@ -2,3 +2,8 @@ export type Room = {
   _id: string;
   nm: string;
 };
+
+export type RoomStore = {
+  rentRoomList: Array<Room>;
+  fetchRoomList: () => void;
+};
