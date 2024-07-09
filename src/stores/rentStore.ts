d353c5596f896155becc432389dcb268ebbc146c
Sam Li <weisen.li@hotmail.com>
2024-07-09 19:32:33 +0000

    update room list response type

--- src/stores/rentStore.ts
+++ src/stores/rentStore.ts
@@ -1,13 +1,15 @@
 import { ROOM_API } from "src/constant/api";
-import { RoomStore } from "src/types/roomType";
+import { RoomListResponse, RoomStore } from "src/types/roomType";
 import { fetchData } from "utils";
 import { create } from "zustand";
 
 const useRentStore = create<RoomStore>()((set) => ({
   rentRoomList: [],
   fetchRoomList: async () => {
-    const response = await fetchData({ url: ROOM_API });
-    set({ rentRoomList: response });
+    const { rooms }: RoomListResponse = await fetchData({ url: ROOM_API });
+    if (Array.isArray(rooms)) {
+      set({ rentRoomList: rooms });
+    }
   },
 }));
 export default useRentStore;
--- src/types/roomType.ts
+++ src/types/roomType.ts
@@ -3,6 +3,10 @@ export type Room = {
   nm: string;
 };
 
+export type RoomListResponse = {
+  rooms: Room[];
+};
+
 export type RoomStore = {
   rentRoomList: Array<Room>;
   fetchRoomList: () => void;
