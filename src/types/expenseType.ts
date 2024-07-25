de52fcf5c7ccb36467cf2b2b2afcbd995ed3df3f
Sam Li <weisen.li@hotmail.com>
2024-07-25 18:52:44 +0000

    add expense type

---
+++ src/types/expenseType.ts
@@ -0,0 +1,23 @@
+export type ExpensesType = {
+  total:string,
+  date:string,
+  categoryPrice:CategoryItem
+}
+
+export type CategoryItem = {
+  category: string;
+  total: string;
+  items: Expense[];
+};
+
+export type CategoryItemProps = {
+  categoryItem: CategoryItem
+};
+
+export type Expense = {
+  id: string;
+  price: string;
+  date: string;
+  category: string;
+  place: any;
+};
