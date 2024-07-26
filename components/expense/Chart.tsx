9f28357f67c45761a79b53bd86a6c5850e62b6a4
Sam Li <weisen.li@hotmail.com>
2024-07-26 02:00:04 +0000

    style bar chart with dimension

--- components/expense/Chart.tsx
+++ components/expense/Chart.tsx
@@ -1,5 +1,8 @@
-import { Text, View } from "react-native";
+import { Dimensions, Text, View } from "react-native";
 import { PieChart, BarChart } from "react-native-gifted-charts";
+
+const {width, height} = Dimensions.get("screen");
+
 export const Chart = ({ totals, expenses }) => {
   const data = expenses.map((categoryPrice) => {
     const priceValue = parseFloat(categoryPrice.total.replace("$", ""));
@@ -9,7 +12,7 @@ export const Chart = ({ totals, expenses }) => {
     return {
       value: priceValue,
       labelComponent: () => (
-        <Text style={{ textAlign: "center", textTransform: "capitalize" }}>
+        <Text style={{fontSize:12, textAlign: "center", textTransform: "capitalize" }}>
           {categoryPrice.category}
         </Text>
       ),
@@ -22,21 +25,19 @@ export const Chart = ({ totals, expenses }) => {
   });
 
   return (
-    <View>
-      <BarChart
-        height={500}
-        width={1000}
-        barWidth={40}
-        noOfSections={6}
-        barBorderRadius={4}
-        //showGradient
-        frontColor={"#1B6BB0"}
-        gradientColor={"#FFEEFE"}
-        data={data}
-        isAnimated
-        yAxisThickness={0}
-        xAxisThickness={0}
-      />
-    </View>
+    <BarChart
+      height={height/2}
+      width={width}
+      barWidth={30}
+      noOfSections={6}
+      barBorderRadius={4}
+      //showGradient
+      frontColor={"#1B6BB0"}
+      gradientColor={"#FFEEFE"}
+      data={data}
+      isAnimated
+      yAxisThickness={0}
+      xAxisThickness={0}
+    />
   );
 };
