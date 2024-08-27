9877860438df1ecf6d12dfff7fd243042ce4ec0d
Sam Li <weisen.li@hotmail.com>
2024-08-27 19:50:15 +0000

    add todo form

--- app/(tabs)/(todos)/TodoList.tsx
+++ app/(tabs)/(todos)/TodoList.tsx
@@ -6,6 +6,7 @@ import {
   FlatList,
   Pressable,
   ScrollView,
+  TextInput,
 } from "react-native";
 import { TODO_API } from "src/constant/api";
 import { fetchData } from "src/utils";
@@ -54,9 +55,20 @@ const TodoList = () => {
           contentContainerStyle={todoStyles.todoList}
         />
       </ScrollView>
-      <Pressable style={todoStyles.todoAddBtn}>
+      <Pressable onPress={()=>setShowForm(true)} style={todoStyles.todoAddBtn}>
         <Text>+</Text>
       </Pressable>
+
+      {showForm &&
+        <View style={todoStyles.todoFormContainer}>
+          <View style={todoStyles.todoForm}>
+            <TextInput placeholder="name" style={todoStyles.todoFormInput} onChangeText={(text)=>setTodo({...todo,name:text})} />
+            <TextInput placeholder="date" style={todoStyles.todoFormInput} onChangeText={(text)=>setTodo({...todo,date:text})} />
+            <Pressable style={todoStyles.todoFormBtn} onPress={()=>setShowForm(false)}>Add</Pressable>
+          </View>
+        </View>
+      }
+      
     </View>
   );
 };
@@ -65,6 +77,35 @@ const todoStyles = StyleSheet.create({
   todoPageContainer: {
     position:'relative',
     flex: 1,
+  },
+  todoFormContainer:{
+    position:'absolute',
+    flex:1,
+    backgroundColor:'rgba(0,0,0,0.5)',
+    justifyContent:'center',
+    alignItems:'center',
+    top:0,
+    left:0,
+    width:'100%',
+    height:'100%'
+  },
+  todoForm:{
+    padding:10,
+    backgroundColor:'#fff',
+    width:'60%',
+    gap:10
+  },
+  todoFormInput:{
+    padding:10,
+    borderWidth:1,
+    borderRadius:10
+  },
+  todoFormBtn:{
+    color:'#fff',
+    backgroundColor:'#369eff',
+    padding:10,
+    textAlign:'center',
+    borderRadius:10
   },
   todoAddBtn:{
     position:'absolute',
