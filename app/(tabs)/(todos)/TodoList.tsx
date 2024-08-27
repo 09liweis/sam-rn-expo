67ddceafef23552e26b88354b4c48d98c08dbeec
Sam Li <weisen.li@hotmail.com>
2024-08-27 19:19:16 +0000

    add delete todo item function

--- app/(tabs)/(todos)/TodoList.tsx
+++ app/(tabs)/(todos)/TodoList.tsx
@@ -1,60 +1,78 @@
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import { TODO_API } from "src/constant/api";
 import { fetchData } from "src/utils";
 const TodoList = () => {
-  
   const [todos, setTodos] = useState([]);
   const fetchTodos = async () => {
     const responses = await fetchData({ url: TODO_API });
     setTodos(responses);
   };
-  const renderTodo = ({item}:any) => {
-    const {_id,name,date,status} = item;
+  const renderTodo = ({ item }: any) => {
+    const { _id, name, date, status } = item;
     return (
-      <Pressable style={todoStyles.todoItem} key={_id}>
+      <Pressable
+        onLongPress={() => handleTodoDelete(_id)}
+        style={todoStyles.todoItem}
+        key={_id}
+      >
         <Text style={todoStyles.todoName}>{name}</Text>
         <Text style={todoStyles.todoDate}>{date}</Text>
       </Pressable>
-    )
-  }
+    );
+  };
 
-  
+  const handleTodoDelete = async (todoId: string) => {
+    const response = await fetchData({
+      url: `${TODO_API}/${todoId}`,
+      method: "DELETE",
+    });
+    fetchTodos();
+  };
+
   useEffect(() => {
     fetchTodos();
   }, []);
 
-  
   return (
-    <View>
-      <Text>Todo List</Text>
+    <ScrollView style={todoStyles.todoPageContainer}>
       <FlatList
         data={todos}
         renderItem={renderTodo}
         contentContainerStyle={todoStyles.todoList}
       />
-    </View>
+    </ScrollView>
   );
 };
 
 const todoStyles = StyleSheet.create({
-  todoList:{
-    padding:10,
+  todoPageContainer: {
     flex: 1,
-    gap:10
   },
-  todoItem:{
+  todoList: {
     padding: 10,
-    borderWidth:1,
-    borderRadius:5,
+    flex: 1,
+    gap: 10,
   },
-  todoName:{
-    fontSize:18,
-    fontWeight:'bold'
+  todoItem: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
   },
-  todoDate:{
-    color:'#ccc'
-  }
+  todoName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  todoDate: {
+    color: "#ccc",
+  },
 });
 
 export default TodoList;
