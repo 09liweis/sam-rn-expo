5ee5f8167e0b36d51f618311ab25db4daf97c0c2
sam <weisen.li@hotmail.com>
2024-12-04 09:57:10 -0500

    delete room

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,26 +6,9 @@ import { primaryColor } from "src/constant/color";
 export default function TabLayout() {
   return (
     <Tabs
-      initialRouteName="(room)"
+      initialRouteName="(expense)"
       screenOptions={{ tabBarActiveTintColor: primaryColor }}
     >
-      <Tabs.Screen
-        name="(room)"
-        options={{
-          // headerRight: () => (
-          //   <Button
-          //     onPress={() => router.push("(room)/form")}
-          //     title="Add Room"
-          //   />
-          // ),
-          headerShown: false,
-          title: "Rooms",
-          tabBarLabel: "Rooms",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
-        }}
-      />
       <Tabs.Screen
         name="(expense)"
         options={{
